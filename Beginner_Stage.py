bf57679dc6ebecd7d64872b320c93c6084bf1d4a
ghost code <116631167+viswaagz@users.noreply.github.com>
2023-06-23 12:41:39 +0530

    Create Beginner_Stage.py

---
+++ Beginner_Stage.py
@@ -0,0 +1,20 @@
+para = """ Hii Guys Intha Program Yaru Perusunu adichu Katu program !!
+Neega ithu la "variables,Ifelse,operators,input statement"
+kathukula tamil iruku.
+"""
+print(para)
+
+a = int(input("A unga Amount Evolo ?"))
+b = int(input("B unga Amount Evolo ?"))
+if a>b:
+    print("a vanthu b vida periyava")
+elif a<b:
+    print("b vanthu a vida periyava")
+elif a==b:
+    print("Rendu perum equal tha...")
+
+""" Vanakam Makala Intha Program la Nee ga Basic operation and control flow Therinjukula.
+First Amount Vachu nee ga yaru perusu nu Therinjukula ithu la para nu use panni irukura variable multi variable store pannu, athu mari
+input process vachu input vangikula also athula ya print iruku. next if athu la last la nee ga elif ku pathila else potu irukula """ 
+
+
